618910fb9c32b3b77fa53a74b0cab6ded6e157e3
AlexandreChaussard <alexandre.chaussard@telecom-sudparis.eu>
2023-03-07 23:07:46 +0100

    RealNVP fix

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,7 +1,9 @@
 import math
 import torch
 import torchvision.transforms as transforms
-
+import torch.distributions as distributions
+import torch.nn.functional as F
+import numpy as np
 
 def sampling(mu, log_var):
     # this function samples a Gaussian distribution,
@@ -72,3 +74,49 @@ def split_feature(tensor, type="split"):
         return tensor[:, : C // 2, ...], tensor[:, C // 2 :, ...]
     elif type == "cross":
         return tensor[:, 0::2, ...], tensor[:, 1::2, ...]
+
+
+def logit_transform(x, constraint=0.9, reverse=False):
+    '''Transforms data from [0, 1] into unbounded space.
+    Restricts data into [0.05, 0.95].
+    Calculates logit(alpha+(1-alpha)*x).
+    Args:
+        x: input tensor.
+        constraint: data constraint before logit.
+        reverse: True if transform data back to [0, 1].
+    Returns:
+        transformed tensor and log-determinant of Jacobian from the transform.
+        (if reverse=True, no log-determinant is returned.)
+    '''
+    if reverse:
+        x = 1. / (torch.exp(-x) + 1.)  # [0.05, 0.95]
+        x *= 2.  # [0.1, 1.9]
+        x -= 1.  # [-0.9, 0.9]
+        x /= constraint  # [-1, 1]
+        x += 1.  # [0, 2]
+        x /= 2.  # [0, 1]
+        return x, 0
+    else:
+        [B, C, H, W] = list(x.size())
+
+        # dequantization
+        noise = distributions.Uniform(0., 1.).sample((B, C, H, W))
+        x = (x * 255. + noise) / 256.
+
+        # restrict data
+        x *= 2.  # [0, 2]
+        x -= 1.  # [-1, 1]
+        x *= constraint  # [-0.9, 0.9]
+        x += 1.  # [0.1, 1.9]
+        x /= 2.  # [0.05, 0.95]
+
+        # logit data
+        logit_x = torch.log(x) - torch.log(1. - x)
+
+        # log-determinant of Jacobian from the transform
+        pre_logit_scale = torch.tensor(
+            np.log(constraint) - np.log(1. - constraint))
+        log_diag_J = F.softplus(logit_x) + F.softplus(-logit_x) \
+                     - F.softplus(-pre_logit_scale)
+
+        return logit_x, torch.sum(log_diag_J, dim=(1, 2, 3))
